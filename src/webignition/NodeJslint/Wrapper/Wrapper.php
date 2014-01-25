94012be86721442baa6be197522a0f421ac0e9ca
Jon Cram <jon@webignition.net>
2014-01-25 10:06:58 +0000

    Set wrapper config

--- src/webignition/NodeJslint/Wrapper/Wrapper.php
+++ src/webignition/NodeJslint/Wrapper/Wrapper.php
@@ -1,10 +1,51 @@
 <?php
 namespace webignition\NodeJslint\Wrapper;
 
+use webignition\NodeJslint\Wrapper\Configuration\Configuration;
+
 
 /**
  * 
  */
-class Wrapper {    
+class Wrapper {
+    
+    
+    /**
+     *
+     * @var Configuration
+     */
+    private $configuration;
+    
+    
+ 
+    /**
+     * 
+     * @param \webignition\NodeJslint\Wrapper\Configuration\Configuration $configuration
+     * @return \webignition\NodeJslint\Wrapper\Wrapper
+     */
+    public function setConfiguration(Configuration $configuration) {
+        $this->configuration = $configuration;
+        return $this;
+    }
+    
+    
+
+    /**
+     * 
+     * @return \webignition\NodeJslint\Wrapper\Configuration\Configuration
+     */
+    public function getConfiguration() {
+        return $this->configuration;
+    }    
+    
+    
+    /**
+     * 
+     * @return boolean
+     */
+    public function hasConfiguration() {
+        return !is_null($this->getConfiguration());
+    }    
+    
     
 }
---
+++ tests/webignition/Tests/NodeJslint/Wrapper/Validate/ValidateTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace webignition\Tests\NodeJslint\Wrapper\Validate;
+
+use webignition\Tests\NodeJslint\Wrapper\BaseTest;
+use webignition\NodeJslint\Wrapper\Wrapper;
+
+class ValidateTest extends BaseTest {
+    
+    public function testTest() {
+        $wrapper = new Wrapper();
+    }
+    
+}
